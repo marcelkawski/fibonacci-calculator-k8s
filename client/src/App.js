aebe77cf9fccbbbffb433515494bd4811c0d690c
marcelkawski <marcel.kawski1@gmail.com>
2023-05-25 12:01:43 +0300

    Modify app title

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 // import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import OtherPage from './OtherPage';
-import Fibonacci from './Fibonacci';
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fibonacci from "./Fibonacci";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1 className="App-title">Fibonacci Calculator 🔢🖩</h1>
+          <h1 className="App-title">Fibonacci Calculator 🖩</h1>
           <Link to="/otherpage">Other Page</Link>
           <div>
             <Route exact path="/" component={Fibonacci} />
